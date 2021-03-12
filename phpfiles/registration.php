c51c0e9b12859f3c0dc5031180c08ca2ea09bf1e
jdeklijn <57561513+jdeklijn@users.noreply.github.com>
2021-03-12 09:31:46 +0100

    Les 5
    Multidimensionale arrays

--- phpfiles/registration.php
+++ phpfiles/registration.php
@@ -14,46 +14,63 @@
 // The variable array()
 
 // Keys values     0       1         2          3
-$aNames = array("Hans","Grietje","Roodkapje","Reus");
-var_dump($aNames);
-echo("<hr/>");
-echo($aNames[3]);
-echo("<hr/>");
-$aName = array();
-$aName['firstname'] = "Frank";
-$aName['insertion'] = "van der";
-$aName['surname'] = "Boom";
-$aName['street'] = "dorpstraat";
-$aName['homenumber'] = 24;
-var_dump($aName);
-echo("<hr/>");
-echo("Uw achternaam is: ".$aName['surname']);
-echo("<hr/>");
-$aDemoVariables = array("string",234523,54.13456,true);
-echo("<hr/>");
-echo("<hr/>");
+//$aNames = array("Hans","Grietje","Roodkapje","Reus");
+//var_dump($aNames);
+//echo("<hr/>");
+//echo($aNames[3]);
+//echo("<hr/>");
+//$aName = array();
+//$aName['firstname'] = "Frank";
+//$aName['insertion'] = "van der";
+//$aName['surname'] = "Boom";
+//$aName['street'] = "dorpstraat";
+//$aName['homenumber'] = 24;
+//var_dump($aName);
+//echo("<hr/>");
+//echo("Uw achternaam is: ".$aName['surname']);
+//echo("<hr/>");
+//$aDemoVariables = array("string",234523,54.13456,true);
+//echo("<hr/>");
+//echo("<hr/>");
+//
+//// An array with fruitstypes
+//$aSoftFruits = array("Bosbessen","Frambozen","Bramen","Rode bessen");
+//$aHardFruits = array("Appel","Peer","Mango","Granaatappel");
+//$aCitrusFruits = array("Sinasappel","Manderijn","Citroen","Grapefruit");
+//// An array filled with fruits
+//$aFruits = array($aSoftFruits,$aHardFruits,$aCitrusFruits);
+//var_dump($aFruits);
+//echo("<hr/>");
+//echo($aFruits[1][3]);
+//echo("<hr/>");
+//foreach($aFruits as $aFruitarray){
+//    echo($aFruitarray[2]."<br/>");
+//} // End foreach fruit
+//echo("<hr/>");
+//for($iCounter = 0;$iCounter<=2;$iCounter++){
+//    for($iKeyCounter=0;$iKeyCounter<=3;$iKeyCounter++){
+//        echo($aFruits[$iCounter][$iKeyCounter]."<br/>");
+//    } // End for keycounter
+//} // End for iCounter
 
-// An array with fruitstypes
-$aSoftFruits = array("Bosbessen","Frambozen","Bramen","Rode bessen");
-$aHardFruits = array("Appel","Peer","Mango","Granaatappel");
-$aCitrusFruits = array("Sinasappel","Manderijn","Citroen","Grapefruit");
-// An array filled with fruits
-$aFruits = array($aSoftFruits,$aHardFruits,$aCitrusFruits);
-var_dump($aFruits);
-echo("<hr/>");
-echo($aFruits[1][3]);
-echo("<hr/>");
-foreach($aFruits as $aFruitarray){
-    echo($aFruitarray[2]."<br/>");
-} // End foreach fruit
-echo("<hr/>");
-for($iCounter = 0;$iCounter<=2;$iCounter++){
-    for($iKeyCounter=0;$iKeyCounter<=3;$iKeyCounter++){
-        echo($aFruits[$iCounter][$iKeyCounter]."<br/>");
-    } // End for keycounter
-} // End for iCounter
+$aFord = array("Ford","Focus","Meriva","Fiesta","Mondeo","Mustang");
+$aOpel = array("Opel","Corsa","Astra","Karl","Insignia","Grandland");
+$aTesla = array("Tesla","Model S","Model X","Roadstar","Model 3","Model T");
+$aPeugeot = array("Peugeot","206","208","306","3008","508","308","608");
+$aSmart = array("Smart","ForTwo","ForFour");
 
+$aCarbase = array($aFord,$aOpel,$aTesla,$aPeugeot,$aSmart);
 
+echo("Mijn type auto is een: ".$aCarbase[2][3]." van het merk: ".$aCarbase[2][0]);
+
+foreach($aCarbase as $aModel){
+    $iTypeCount = count($aModel);
+    echo("<hr/>De typen van merk: ".$aModel[0]."<select name='".$aModel[0]."'>");
+    for($iModelCounter = 1;$iModelCounter<$iTypeCount;$iModelCounter++){        
+     echo("<option value='".$iModelCounter."'>$aModel[$iModelCounter]</option>");
+    }
+    echo("</select>");
+} // End foreach carbase
 
 // ======================= File handling ==========================
 
